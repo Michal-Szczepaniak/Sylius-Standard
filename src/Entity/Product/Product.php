a5e1a7399bf2e69d0676f90818953330e0d14b12
Michal-Szczepaniak <m.szczepaniak.000@gmail.com>
2020-03-10 21:07:29 +0100

    Remove unused ProductTranslation

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
-use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
-use Sylius\Component\Product\Model\ProductTranslationInterface;
 
 class Product extends BaseProduct
 {
@@ -20,11 +18,6 @@ class Product extends BaseProduct
      * @var int
      */
     protected $color;
-
-    protected function createTranslation(): ProductTranslationInterface
-    {
-        return new ProductTranslation();
-    }
 
     public function getColor()
     {
